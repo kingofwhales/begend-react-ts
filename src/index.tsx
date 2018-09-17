2bc8a5f0b7c3e148b14735a9b9a3de858102c385
kingofwhales <wfan5@illinois.edu>
2018-09-17 15:50:14 +0800

    FEATURE: typescript react starter finished. switched to redux example

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,19 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import Hello from './components/StatefulHello';
-import './index.css';
-import registerServiceWorker from './registerServiceWorker';
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+import Hello from "./containers/Hello";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import { enthusiasm } from "./reducers/index";
+
+const store = createStore(enthusiasm, {
+  enthusiasmLevel: 1,
+  languageName: "TypeScript"
+});
 
 ReactDOM.render(
-  <Hello name="TypeScript" enthusiasmLevel={100} />,
+  <Provider store={store}>
+    <Hello />
+  </Provider>,
   document.getElementById("root") as HTMLElement
 );
-registerServiceWorker();
