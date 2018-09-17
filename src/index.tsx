ab8f2962746b78a1dcce490eb4ecebf6ef0133cf
kingofwhales <wfan5@illinois.edu>
2018-09-17 15:01:56 +0800

    FEATURE: added enzyme for testings

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import App from './App';
+import Hello from './components/StatefulHello';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root') as HTMLElement
+  <Hello name="TypeScript" enthusiasmLevel={100} />,
+  document.getElementById("root") as HTMLElement
 );
 registerServiceWorker();
